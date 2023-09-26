401819802f03363aacf47fd27c89c96e0eac80b2
KitSarmiento <ksarmiento020@gmail.com>
2023-09-25 21:43:04 -0400

    Added a route using the get method.

--- server.js
+++ server.js
@@ -4,3 +4,9 @@ const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Routes
+app.get("/api/notes", (req, res) => {
+  const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+  res.json(notes);
+});
